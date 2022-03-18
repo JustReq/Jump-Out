4db590407efd999142dee07560fbb4768e77c411
Req <justreqofficial@gmail.com>
2022-03-17 23:07:23 -0400

    Where's the waddle

--- Scripts/PlayerCharacter.gd
+++ Scripts/PlayerCharacter.gd
@@ -2,41 +2,67 @@ extends KinematicBody2D
 
 var inputVector
 
-var acceleration = 1200
-var speed = 240
-var friction = 1200
+const WALKSPEED = 320
+const WALKACCELERATION = 1000
+const RUNSPEED = 720
+const RUNACCELERATION = 850
+
+const DECCELERATIONFACTOR = 2
+
+var speed
+var acceleration
+var friction = 2000
 
 var terminalSpeed = 720
 var gravity = 1500
-var jumpSpeed = 750
+var jumpSpeed = 800
 
 var velocity = Vector2.ZERO
 
 var isGrounded
 var isJumping
 
+const CAMERAZOOMTHRESHOLD = 600
+
+onready var playerCharacterInfoLabel = $PlayerCharacterInfoLabel
+onready var playerCharacterCamera = $PlayerCharacterCamera
 onready var playerCharacterSprite = $PlayerCharacterSprite
+onready var playerCharacterCollider = $PlayerCharacterCollider
 onready var playerCharacterAnimator = $PlayerCharacterAnimationTree.get("parameters/playback")
 onready var playerCharacterAnimationController = $PlayerCharacterAnimator
-
 onready var playerCharacterGroundedRayCast = $PlayerCharacterGroundedRayCast
 
 func _physics_process(delta):
 	if (Input.is_action_pressed("reset")):
 		get_tree().reload_current_scene()
 	
+	playerCharacterInfoLabel.text = "speed: {speed}\ngrounded?: {grounded}\nacceleration: {acceleration}".format({
+		"speed":Vector2(floor(velocity.x), floor(velocity.y)),
+		"grounded":isGrounded,
+		"acceleration": acceleration
+		})
+	
 	movement_WalkAndRun(delta)
 	movement_GravityAndJump(delta)
 	
+	cameraController(delta)
 	playerCharacterAnimations()
 
 func movement_WalkAndRun(var deltaTime):
 	inputVector = Input.get_action_strength("walk_right") - Input.get_action_strength("walk_left")
 	
 	if Input.is_action_pressed("run"):
-		speed = 480
+		speed = RUNSPEED
 	else:
-		speed = 240
+		speed = WALKSPEED
+	
+	if abs(velocity.x) > WALKSPEED:
+		acceleration = RUNACCELERATION
+	else:
+		acceleration = WALKACCELERATION
+	
+	if (inputVector >= 0 && velocity.x < 0) || (inputVector <= 0 && velocity.x > 0):
+		acceleration *= DECCELERATIONFACTOR
 	
 	if inputVector != 0:
 		velocity = velocity.move_toward(Vector2(inputVector * speed, velocity.y), acceleration * deltaTime)
@@ -57,24 +83,35 @@ func movement_GravityAndJump(var deltaTime):
 		if velocity.y < -100:
 			velocity.y = -200
 
+func cameraController(var deltaTime):
+	if (abs(velocity.x) > CAMERAZOOMTHRESHOLD):
+		playerCharacterCamera.zoom = playerCharacterCamera.zoom.move_toward(Vector2(1.6, 1.6), (abs(velocity.x) - CAMERAZOOMTHRESHOLD) / (RUNSPEED - CAMERAZOOMTHRESHOLD) * deltaTime / 10)
+	else:
+		playerCharacterCamera.zoom = playerCharacterCamera.zoom.move_toward(Vector2(0.8, 0.8), -(abs(velocity.x) - CAMERAZOOMTHRESHOLD) / (RUNSPEED - CAMERAZOOMTHRESHOLD) * deltaTime / 10)
+
 func playerCharacterAnimations():
 	if velocity.x < 0:
 		playerCharacterSprite.flip_h = true
 	elif velocity.x > 0:
 		playerCharacterSprite.flip_h = false
 	
-	if !isJumping:
+	if isGrounded:
 		if velocity.x == 0:
-			playerCharacterAnimator.travel("Idle")
+			if Input.is_action_pressed("crouch"):
+				playerCharacterAnimator.travel("Crouch")
+			else:
+				playerCharacterAnimator.travel("Idle")
 		else:
 			if Input.is_action_pressed("run"):
 				playerCharacterAnimator.travel("Run")
 			else:
-				playerCharacterAnimator.travel("Walk")
+				if (inputVector > 0 && velocity.x < 0) || (inputVector < 0 && velocity.x > 0):
+					playerCharacterAnimator.travel("Turn_Walk")
+				else:
+					playerCharacterAnimator.travel("Walk")
 		
-		if velocity.y > 0 && !isGrounded:
+		if Input.is_action_pressed("jump") && velocity.y < 0:
+			playerCharacterAnimator.travel("Jump")
+	else:
+		if velocity.y > 0:
 			playerCharacterAnimator.travel("Fall")
-
-
-	if Input.is_action_just_pressed("jump"):
-		playerCharacterAnimator.travel("Jump")
